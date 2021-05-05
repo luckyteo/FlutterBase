de79d212e498ae93af9c342d6f3ec5af94d3db3d
devsimple <thetai180491@gmail.com>
2021-05-05 16:20:02 +0700

    test datasource

--- lib/core/platform/network_info.dart
+++ lib/core/platform/network_info.dart
@@ -1,3 +1,14 @@
+import 'package:connectivity/connectivity.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+
+  @override
+  Future<bool> get isConnected async {
+    ConnectivityResult result = await Connectivity().checkConnectivity();
+    return Future.value(result != ConnectivityResult.none);
+  }
 }
--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_base_clean_architecture/features/number_trivia/domain/re
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter_base_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:flutter_base_clean_architecture/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:flutter_base_clean_architecture/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
